e0fb516c79d13c600a91d0332ad2d63cd6f5d149
Ashutosh Panigrahy <ashutoshp.iitkgp@gmail.com>
2026-03-14 15:01:55 +0530

    finalize send/recv fncs

--- common_utils.c
+++ common_utils.c
@@ -22,16 +22,86 @@ int obtain_ip_list(const char *hostname, const char *portno,
   return PROG_SUCCESS;
 }
 
-int receive_byte_stream(const int fd) {
-  char incoming_buffer[RECEIVE_BUFFER_SIZE];
+ssize_t receive_all(const int fd, void *incoming_buffer,
+                    size_t incoming_buffer_size) {
 
-  ssize_t recv_size = 0;
-  while ((recv_size = recv(fd, (void *)incoming_buffer,
-                           sizeof(incoming_buffer) - 1, 0)) > 0) {
-    incoming_buffer[recv_size] = '\0';
-    log_output(incoming_buffer);
-    log_output("------------------\n\n");
+  ssize_t total_recvd = 0;
+
+  while (total_recvd < incoming_buffer_size) {
+    ssize_t recv_byte_count = recv(fd, (char *)(incoming_buffer) + total_recvd,
+                                   incoming_buffer_size - total_recvd, 0);
+
+    // NOTE: error / peer closed connection
+    if (recv_byte_count <= 0) {
+      return -1;
+    }
+
+    total_recvd += recv_byte_count;
   }
 
-  return ((recv_size == 0) ? PROG_SUCCESS : PROG_FAILURE);
+  return total_recvd;
+}
+
+ssize_t receive_byte_stream(const int fd, char *buffer) {
+
+  // NOTE: obtain message size first
+  uint32_t incoming_buffer_size = 0;
+  ssize_t size_byte_count =
+      receive_all(fd, (void *)&incoming_buffer_size, sizeof(uint32_t));
+
+  // NOTE: convert from network to host byte order (endianess check)
+  incoming_buffer_size = ntohl(incoming_buffer_size);
+  if (size_byte_count <= 0 || incoming_buffer_size + 1 > BUFFER_SIZE) {
+    return -1;
+  }
+
+  // NOTE: obtain actual message
+  ssize_t total_recvd = receive_all(fd, buffer, incoming_buffer_size);
+  if (total_recvd <= 0) {
+    return -1;
+  }
+  buffer[total_recvd] = '\0';
+
+  return total_recvd + 1;
+}
+
+ssize_t send_all(const int fd, const void *outgoing_buffer,
+                 size_t outgoing_buffer_size) {
+  ssize_t total_sent = 0;
+
+  while (total_sent < outgoing_buffer_size) {
+    ssize_t sent_byte_count =
+        send(fd, (const char *)(outgoing_buffer) + total_sent,
+             outgoing_buffer_size - total_sent, 0);
+
+    // NOTE: error / peer closed connection
+    if (sent_byte_count <= 0) {
+      return -1;
+    }
+
+    total_sent += sent_byte_count;
+  }
+
+  return total_sent;
+}
+
+ssize_t send_byte_stream(const int fd, const char *outgoing_buffer,
+                         size_t outgoing_buffer_size) {
+
+  // NOTE: send out the message size
+  uint32_t len = htonl(outgoing_buffer_size);
+  ssize_t size_byte_count = send_all(fd, (void *)&len, sizeof(uint32_t));
+
+  if (size_byte_count <= 0) {
+    return -1;
+  }
+
+  // NOTE: send the actual message
+  ssize_t total_sent =
+      send_all(fd, (const void *)(outgoing_buffer), outgoing_buffer_size);
+  if (total_sent <= 0) {
+    return -1;
+  }
+
+  return total_sent;
 }
--- common_utils.h
+++ common_utils.h
@@ -15,7 +15,7 @@
 #include <netinet/in.h>
 
 #define SERVER_PORT "8080"
-#define RECEIVE_BUFFER_SIZE 8 * 1024
+#define BUFFER_SIZE 8 * 1024
 
 #define PROG_SUCCESS 0
 #define PROG_FAILURE -1
@@ -28,6 +28,12 @@ int obtain_ip_list(const char *hostname, const char *portno,
                    const struct addrinfo *hints,
                    struct addrinfo **ptr_to_ip_list);
 
-int receive_byte_stream(const int fd);
+ssize_t receive_all(const int fd, void *incoming_buffer,
+                    size_t incoming_buffer_size);
+ssize_t receive_byte_stream(const int fd, char *buffer);
+
+ssize_t send_all(const int fd, const void *outgoing_buffer,
+                 size_t outgoing_buffer_size);
+ssize_t send_byte_stream(const int fd, const char *buffer, size_t buffer_size);
 
 #endif // GRP_CHAT_UTILS
